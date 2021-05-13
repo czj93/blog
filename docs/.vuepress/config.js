3521c540427e0c8c4bb6a32f6c8f93938f91f32d
e <1205234934@qq.com>
2021-05-13 20:52:24 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,32 @@ module.exports = {
   title: 'Czj的博客',
   description: '',
   theme: '@vuepress/blog',
+  base: '/blog/',
   themeConfig: {
-
+    smoothScroll: true,
+    dateFormat: 'YYYY-MM-DD',
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    footer: {
+      contact: [
+        {
+          type: 'mail',
+          link: 'mailto:1205234934@qq.com',
+        }
+      ],
+      copyright: [
+        {
+          text: 'MIT Licensed | Copyright © 2021',
+        }
+      ]
+    }
   },
-  base: '/blog/'
 }
