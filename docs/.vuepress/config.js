54d115d5e2f54d00f72539f0a6f2fc53ea28b485
e <1205234934@qq.com>
2021-06-08 23:03:54 +0800

    ✨ feat: 添加百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,17 @@ module.exports = {
   description: '',
   theme: '@vuepress/blog',
   base: '/blog/',
+  head: [
+    [ 'script', { type: 'text/javascript' }, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?24612437a0e31159f0220c5180f93f1c";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    ` ]
+  ],
   themeConfig: {
     smoothScroll: true,
     dateFormat: 'YYYY-MM-DD',
